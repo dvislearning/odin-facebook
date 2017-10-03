44c4880fd80bc41b6bb14bbe6a0842f2c345c660
DV <dvislearning@gmail.com>
2017-10-03 07:58:43 +0000

    establish layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/new'
+  root 'static_pages#new'  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
