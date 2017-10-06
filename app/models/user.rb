af35a1108fedb355ff5a2fe6d04a173c5e6000a0
DV <dvislearning@gmail.com>
2017-10-06 08:37:56 +0000

    add model validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # :validatable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable
+  validates :email, presence: true, length: { maximum: 255 },
+                    uniqueness: { case_sensitive: false }
+  validates :username,  presence: true, length: { maximum: 50 }         
 end
