aca20977b944712425ed2c154f99570e784ee9d6
DV <dvislearning@gmail.com>
2017-10-08 02:34:46 +0000

    setup User controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/new'
   root 'static_pages#new'  
   devise_for :users
+  resources :users, only: [:show, :index]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
