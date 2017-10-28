8d6c38b736e84f2883589580bd5d05cde9d76df2
DV <dvislearning@gmail.com>
2017-10-28 08:36:46 +0000

    write post form and post delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,30 @@ class PostsController < ApplicationController
   
   def index
     @posts = current_user.timeline.paginate(page: params[:page])
+    @post = current_user.posts.build
   end
   
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = "Post Saved!"
+      redirect_to posts_url
+    else
+      flash[:notice] = "Unable to Save Post"
+      redirect_to posts_url
+    end
   end
   
   def destroy
+    @post = current_user.posts.find_by(id: params[:id])
+    @post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to posts_url
   end
+  
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end  
 end
