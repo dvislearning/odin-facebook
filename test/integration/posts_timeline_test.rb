11d19a891371b936b758a8d7d2028dc79e1970b2
DV <dvislearning@gmail.com>
2018-02-03 07:15:44 +0000

    write tests for timeline display

--- test/integration/posts_timeline_test.rb
+++ test/integration/posts_timeline_test.rb
@@ -6,13 +6,21 @@ class PostsTimelineTest < ActionDispatch::IntegrationTest
   def setup
     sign_in users(:example)
     @user = users(:example)
+    @example_post = posts(:middle_hand) # Post by Example_user
+    @red_post = posts(:left_hand) # Post by Example's Friend    
+    @yellow_post = posts(:yellow_post) # Post by someone not Example's Friend
   end
   
   test "timeline display" do
     get posts_path
     assert_template 'posts/index'
+    assert_match @example_post.content, response.body
+    assert_match @red_post.content, response.body
+    assert_no_match @yellow_post.content, response.body
     @user.timeline.paginate(page: 1).each do |post|
       assert_match post.content, response.body
     end
   end  
 end
+
+# Write tests checking if friends and non friends appear on timeline.
