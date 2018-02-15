282dd4b7452255abe455ab6e29d9c49944769a92
DV <dvislearning@gmail.com>
2018-02-15 09:40:05 +0000

    render comments with posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   validates :user_id,  presence: true
-  validates :content,  presence: true, length: { maximum: 5001 }  
+  validates :content,  presence: true, length: { maximum: 5001 }
   
+  # Over-rode this method to avoid generating comment controller.  Reconsider refactoring.
+  def to_partial_path
+    'posts/comment'
+  end  
 end
